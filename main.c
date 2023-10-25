ae8ee6f26feffe075e95611e00ee4106463c0a2e
elias-men <135940883+elias-men@users.noreply.github.com>
2023-10-26 01:19:21 +0200

    ajout de methode img_dist

--- main.c
+++ main.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <regex.h>
+#include "vectors/vector.h"
 
 #define MAX_IMAGE_NAME_LENGTH 999
 
 void Process_args(char* img_path, int argc, char* argv[]);
+
+int img_dist(char* path_comp[] , char* path_img[]);
 
 /**
 *Doit recevoir comme argument la Photo a compareet comme second 
@@ -16,27 +19,47 @@ void Process_args(char* img_path, int argc, char* argv[]);
 *@return L'image la plus similaire (ex. Most similar image found: 'img/22.bmp' with a distance of 12.)
 *
 **/
-int main(int argc, char* argv[]) {
-   char *image_to_compare;
-   image_to_compare= (char *)malloc(sizeof(char)*MAX_IMAGE_NAME_LENGTH);
-   if (image_to_compare == NULL) {
-      fprintf(stderr, "Erreur d'allocation mémoire\n");
-      exit(EXIT_FAILURE);
-   }
-   Process_args(image_to_compare, argc, argv);
-
-
-   free(image_to_compare);
-   return 0;
-
+int main(int argc, char* argv[]){
+    char *image_to_compare;
+    image_to_compare= (char *)malloc(sizeof(char)*MAX_IMAGE_NAME_LENGTH);
+    if (image_to_compare == NULL) {
+    fprintf(stderr, "Erreur d'allocation mémoire\n");
+    exit(EXIT_FAILURE);
 }
 
+    Process_args(image_to_compare, argc, argv);
+    
+    
+    int dist=img_dist(image_to_compare , "img/1.bmp");
+    
+    printf("%d", dist );
+
+
+    free(image_to_compare);
+    return 0;
+}
 
 void Process_args(char* image_to_compare, int argc, char* argv[]){
-   if (argc!=2){
+
+    if (argc!=2)
+    {
+
         printf("nombre d'argument incorecte");
         exit(1);        
-   }  else {
+        
+    }
+    else
+    {
         strcat(image_to_compare,argv[1]);
-   }
+    }
+
+    
+
+    
+}
+int img_dist(char* path_comp[] , char* path_img[]){
+    char command[256];
+    snprintf(command, sizeof(command),"img-dist/img-dist %s %s",path_comp , path_img);
+    int retour = system(command);
+    return (retour/256);
 }
